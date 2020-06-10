4c8ddfc232ab693cd218858c0a6919b02bf75335
flrizzato <fernando.rizzato@gmail.com>
2020-06-10 15:24:17 -0300

    10.4

---
+++ 10.4-Sydney/HighDPIImageList/HighDPIImageList.dpr
@@ -0,0 +1,14 @@
+program HighDPIImageList;
+
+uses
+  Vcl.Forms,
+  HighDPIImageListUnit in 'HighDPIImageListUnit.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
